494b842718babf12d3c8e163dbbc3063904fb80b
intars-cyber <intars.apinis@gmail.com>
2025-03-28 08:52:54 +0000

    Revert to /stream for Koyeb simplicity

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-app.use('/proxy', (req, res, next) => {
-  console.log('Middleware hit for /proxy');
-  if (req.path === '/stream') next();
-  else res.redirect(302, 'https://bradio.dev');
-});
-
-app.get('/proxy/stream', (req, res) => {
+app.get('/stream', (req, res) => {
   let streamUrl = req.query.url;
-  if (!streamUrl) return res.redirect(302, 'https://bradio.dev');
+  if (!streamUrl) return res.status(400).send('No stream URL provided');
 
   if (!streamUrl.match(/^https?:\/\//)) streamUrl = `https://${streamUrl}`;
 
   console.log(`Requesting stream: ${streamUrl}`);
-  // Set headers immediately to start response
   res.set({
     'Content-Type': 'audio/aac',
     'Access-Control-Allow-Origin': 'https://bradio.dev',
     'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive' // Ensure streaming
+    'Connection': 'keep-alive'
   });
 
   const stream = request({
@@ -32,8 +25,7 @@ app.get('/proxy/stream', (req, res) => {
 
   stream.on('response', (resp) => {
     console.log(`Stream response: ${resp.statusCode}, Content-Type: ${resp.headers['content-type']}`);
-    console.log('Piping stream to response');
-    stream.pipe(res, { end: true }); // Explicitly end response with stream
+    stream.pipe(res, { end: true });
   });
 
   stream.on('error', (err) => {
@@ -44,7 +36,7 @@ app.get('/proxy/stream', (req, res) => {
 
 app.get('/', (req, res) => {
   console.log('Root endpoint hit');
-  res.send('BRadio Proxy Running - Use /proxy/stream');
+  res.send('BRadio Proxy Running - Use /stream?url=<stream-url>');
 });
 
 const port = process.env.PORT || 3000;
